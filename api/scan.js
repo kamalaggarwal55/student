493e90045f142e2654470ea290390c6d73263620
kamalaggarwal55 <151158156+kamalaggarwal55@users.noreply.github.com>
2026-09-22 23:53:17 +0530

    api/scan.js

--- api/scan.js
+++ api/scan.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         }
 
         // 🔑 YAHAN APNI GEMINI API KEY RAKHEIN (Yeh server par secure rahegi)
-        const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "YAHAN_APNI_REAL_API_KEY_DAAL_DENA";
+        const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "AQ.Ab8RN6I0sNVSNmnapJVvNYp_NSpWXHJ74rlNcILh8HxioqtGPw";
 
         const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`, {
             method: 'POST',
